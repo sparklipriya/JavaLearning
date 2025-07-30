ca10df15799c132f1b807080a813de1b7a57b6b2
sparklipriya <priyayadav180016@gmail.com>
2025-07-30 16:06:34 +0530

    Initial

---
+++ src/Introduction/App.java
@@ -0,0 +1,25 @@
+package Introduction;
+
+public class App{
+    public static void main(String[] args){
+        System.out.println("Hello ");
+        System.out.println("Pattern 1");
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        System.out.println("* * * * *");
+        System.out.println("Pattern 2");
+        System.out.println("* * * * *");
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*");
+        System.out.println("Pattern 3");
+        System.out.println("        *");
+        System.out.println("      * *");
+        System.out.println("    * * *");
+        System.out.println("  * * * *");
+        System.out.println("* * * * *");
+    }
+}
---
+++ src/datatype_variable_input/App.java
@@ -0,0 +1,26 @@
+package datatype_variable_input;
+
+public class App {
+    public static void main(String[] args) {
+
+        // variable decleration
+        int myNum = 865;
+        int youNum=7;
+        System.out.println(youNum);
+        youNum = 45;
+        System.out.println(youNum);
+        boolean isVegetarian = false;
+        System.out.println(isVegetarian);
+        float myFloat = 5;
+        float f = 0.5f;
+        System.out.println(myFloat);
+        System.out.println(f);
+        double d = 5.3464;
+        System.out.println(d);
+        String wish = " Happy Day Ahead !";
+        System.err.println(wish);
+        char c = 'w';
+        System.out.println(c);
+
+    }
+}
